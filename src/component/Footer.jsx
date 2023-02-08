56a346a309e6fb4e2b3536a819f16fcf973bb320
Emmanuel Essien <ehmaessien@gmail.com>
2023-02-08 01:28:57 +0100

    Update Font-sizes

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -6,10 +6,10 @@ function Footer() {
       <div className="container px-4 md:mx-auto">
         <div className="flex flex-col md:flex-row md:justify-between">
           <div>
-            <h4 className="text-2xl font-[674]">Skill-Up Aks</h4>
-            <p>Skill-up@Copyright</p>
+            <h4 className="text-[18px] md:text-2xl font-[674]">Skill-Up Aks</h4>
+            <p className="text-[12px] md:text-base">Skill-up@Copyright</p>
           </div>
-          <div className="mt-[64px] block md:mt-0 md:flex gap-x-6">
+          <div className="text-sm md:text-base mt-[64px] md:mt-0 flex gap-x-6">
             <p>Privacy Policy</p>
             <p>Terms and condition</p>
           </div>
